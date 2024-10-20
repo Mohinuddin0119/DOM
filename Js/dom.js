873b0d184497b6147dc8496f4c85a769e436009a
Mohin uddin <Mohinuddin0119@gmail.com>
2024-10-20 11:26:30 +0600

    added add remove style properties

--- Js/dom.js
+++ Js/dom.js
@@ -56,7 +56,7 @@ console.log(dom1.classList)
 // add attribute
 const add = dom1.classList.add('second-class');
 console.log(dom1.classList)
-*/
+
 // set attribute
 // document.getElementById('dom').setAttribute('style', 'background-color:blue;')
 // document.getElementById('dom').setAttribute('style', 'color:blue;')
@@ -72,3 +72,23 @@ console.log(dom2[0].innerHTML)
 console.log(dom2[0].innerText)
 // change
 // console.log(dom2[0].innerText = 'I am Mahi')
+*/
+// dom style properties
+const sections = document.querySelectorAll('section');
+// console.log(sections)
+for(const section of sections){
+    // console.log(section)
+    section.style.border = '3px solid steelblue'
+    section.style.marginBottom = '5px'
+    section.style.paddingLeft = '5px'
+    section.style.backgroundColor = 'lightgrey'
+}
+// 
+const placesContainer = document.getElementById('places-container') 
+placesContainer.style.backgroundColor = 'pink'
+// add
+const domContainer = document.getElementById('dom-head')
+domContainer.classList.add('yellow-bg')
+domContainer.classList.add('text')
+// remove
+domContainer.classList.remove('text')
